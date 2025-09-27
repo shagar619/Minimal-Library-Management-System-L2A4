cf0453040f728c01936d99bb7116921921938212
shagar619 <ashagar619@gmail.com>
2025-09-27 23:22:33 +0600

    added redux set up

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
+import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router'
 import router from './routes/Router.tsx'
 import { ThemeProvider } from './providers/theme-provider.tsx'
+import { store } from './redux/store.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     
     <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-      <RouterProvider router={router}>
+
+    <Provider store={store}>
+
+    <RouterProvider router={router}>
 
       </RouterProvider>
+
+    </Provider>
+
     </ThemeProvider>
 
   </StrictMode>,
